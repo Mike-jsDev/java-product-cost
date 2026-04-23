aaba3158be7e8abb48dd07dec2e01c8a3cfe3d35
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-04-23 16:41:05 +0200

    init homework project

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
+package app;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Start project");
+        System.out.println("Homework 5 running");
     }
 }
