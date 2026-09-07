fd66077015da4144ac35f4cb8c8d6dfd1d217bb0
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 15:39:17 +0200

    home work 14

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,12 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Homework 5 running");
+        DataRepository repository = new DataRepository();
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.formListOutput(repository.getData()));
+        uiOperator.getOutput(handler.formOutput(repository.getData(), 2));
     }
 }
