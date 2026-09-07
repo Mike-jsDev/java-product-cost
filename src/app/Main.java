e868ad844972ee2c5acb6dc673e7eae21fd98536
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 16:59:46 +0200

    home work 26

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,14 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Homework 5 running");
+        User user = new User("Alice");
+        Address address = new Address("Khreshchatyk 1", "Kyiv", "Ukraine");
+
+        user.setAddress(address);
+
+        System.out.println("User: " + user.getName());
+        System.out.println("Address: " + user.getAddress());
     }
 }
---
+++ src/app/User.java
@@ -0,0 +1,27 @@
+package app;
+
+public class User {
+
+    private String name;
+    private Address address;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
