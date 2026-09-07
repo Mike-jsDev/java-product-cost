80b56cdbffa58994b563d28fb25c95f1333610ba
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 15:58:45 +0200

    home work 15

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,11 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Homework 5 running");
+        DataHandler handler = new DataHandler();
+        UIOperator uiOperator = new UIOperator();
+        uiOperator.getOutput(handler.getAll());
+        uiOperator.getOutput(handler.getById(172));
     }
 }
