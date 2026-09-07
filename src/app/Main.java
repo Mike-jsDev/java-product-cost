206a085d67c18166c9edd00f87ea5a170276e45d
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 14:33:01 +0200

    home work 9

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,21 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Homework 5 running");
+        DataProvider provider = new DataProvider();
+        DataHandler dataHandler = new DataHandler();
+
+        // Processing product names array
+        String namesOutput = dataHandler.handleData(provider.getProductNames());
+        getOutput("Products: " + namesOutput);
+
+        // Processing sales amounts array
+        String salesOutput = dataHandler.handleData(provider.getSalesAmounts());
+        getOutput("Sales, EUR: " + salesOutput);
+    }
+
+    private static void getOutput(String output) {
+        System.out.println(output);
     }
 }
