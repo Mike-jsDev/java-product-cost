10118ac538eb06a2c6ebcf05ab663161dc158969
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 16:43:35 +0200

    home work 23

---
+++ password-app/src/main/java/app/Main.java
@@ -0,0 +1,12 @@
+package app;
+
+import com.example.PasswordGenerator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int passwordLength = 12;
+        String generatedPassword = PasswordGenerator.generatePassword(passwordLength);
+        System.out.println("Згенерований пароль: " + generatedPassword);
+    }
+}
