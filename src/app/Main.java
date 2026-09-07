4f7e944e70659f25ec6821a39720d3f8b95c3ab2
Solamatin Mikhail <solamatinm@smart-ui.pro>
2026-09-07 16:49:37 +0200

    home work 24

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,7 +1,24 @@
 package app;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Homework 5 running");
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        System.out.println("Same instance: " + (logger1 == logger2));
+
+        logger1.log("Application started");
+        doWork();
+        logger2.log("Application finished");
+
+        System.out.println("Total logs: " + logger1.getLogs().size());
+        System.out.println("All logs: " + logger1.getLogs());
+    }
+
+    private static void doWork() {
+        Logger logger = Logger.getInstance();
+        logger.log("Processing data...");
+        logger.log("Data processed successfully");
     }
 }
